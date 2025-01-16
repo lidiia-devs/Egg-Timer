6a1cb2675fa67b7a9a83d49a540457d4d3d1fc14
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-01-16 03:26:16 -0500

    last commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,35 +7,62 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let eggTimer = ["Soft": 300, "Medium": 420, "Hard": 720]
-    
-    var secondsRemaining = 60
+    let eggTimer = ["Soft": 3, "Medium": 420, "Hard": 720]
     
     var timer = Timer()
-    @IBOutlet weak var titleLable: UILabel!
+    var totalTime = 0
+    var secondsPassed = 0
     
-    @IBAction func hardnessSelected(_ sender: UIButton) {
+    //MARK: Outlets
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton)  {
         
         timer.invalidate() // stops and runs from scratch
         
         let hardness = sender.currentTitle! //Soft, Medium, Hard
         
-         secondsRemaining = eggTimer[hardness]!
+        totalTime = eggTimer[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        timerLabel.text = hardness
        
          timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
     @objc func updateCounter() {
         //example functionality
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds")
-            secondsRemaining -= 1
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+           // print(progressBar.progress)
+    
         } else {
-            titleLable.text = "DONE!"
+            playSound()
+            timerLabel.text = "DONE!"
             timer.invalidate()
+        }
+    }
+    
+    var player: AVAudioPlayer?
+
+    func playSound() {
+        guard let path = Bundle.main.path(forResource: "alarm_sound", ofType:"mp3") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch {
+            print("There is an issue with this code!")
         }
     }
     
